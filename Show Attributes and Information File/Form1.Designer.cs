7cfb2a082b4f936c38e668807504d364cbc8db01
Phuclatui <mydreamand202@gmail.com>
2026-06-09 10:43:53 +0700

    Update

---
+++ Show Attributes and Information File/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace FileAndDir1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbIn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbIn
+            // 
+            this.tbIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbIn.Location = new System.Drawing.Point(141, 73);
+            this.tbIn.Name = "tbIn";
+            this.tbIn.Size = new System.Drawing.Size(519, 22);
+            this.tbIn.TabIndex = 0;
+            this.tbIn.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbIn_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter the path File or Directory:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 128);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(519, 310);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbIn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Show Attributes and Information File/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace FileAndDir1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public void GetIn4(string FileName)
+        {
+            textBox1.Clear();
+
+            textBox1.Text = Path.GetFileName(FileName) + "\r\n";
+            textBox1.AppendText("Created: " + File.GetCreationTime(FileName) + "\r\n");
+            textBox1.AppendText("Last Modifiers: " + File.GetLastWriteTime(FileName) + "\r\n");
+            textBox1.AppendText("Last Accessed: " + File.GetLastAccessTime(FileName) + "\r\n");
+        }
+
+        private void tbIn_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox1.Clear();
+
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                string FileName = @tbIn.Text;
+
+                if (File.Exists(FileName))
+                {
+                    GetIn4(FileName);
+                    StreamReader stream = new StreamReader(FileName);
+                    try
+                    {
+                        textBox1.AppendText(stream.ReadToEnd());
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    finally
+                    {
+                        stream.Close();
+                    }
+                }
+
+                else if (Directory.Exists(FileName))
+                {
+                    string[] arrDir = Directory.GetFiles(FileName);
+                    textBox1.AppendText("Directory contents: " + "\r\n");
+                    foreach (var file in arrDir)
+                    {
+                        textBox1.AppendText(file + "\r\n");
+                    }
+
+                }
+
+                else
+                    MessageBox.Show(tbIn.Text + " does not exist", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+
+
+
+
+}
